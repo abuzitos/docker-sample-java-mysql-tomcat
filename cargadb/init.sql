b13ee385386ff33de95f7839aede8cde1ca974c4
Nelson Abu Samra Rahal Junior <abuzitos@gmail.com>
2019-11-26 06:49:34 -0300

    app completa

--- cargadb/init.sql
+++ cargadb/init.sql
@@ -1,3 +1,4 @@
 create database javatest;
+use javatest;
 create table testdata (id int not null auto_increment primary key,foo varchar(25),bar int);
 insert into testdata values(null, 'hello', 12345);
